405c12a00052bda7a89a6217e989b1d963f8c160
taehwajang <61370482+taehwajang@users.noreply.github.com>
2022-04-13 00:26:48 +0900

    add : 체크박스 선택 후 삭제 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,97 @@
-import { Button, Input } from '@mui/material';
+import { Button } from "@mui/material";
 
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import Header from './component/header';
-import { ChineseFood, JapaneseFood, KoreanFood, snackFood, users, nextFood } from './data/data';
+import React, { useState } from "react";
+import styled from "styled-components";
+import Header from "./component/header";
+import { nextFood } from "./data/data";
 
 const App = () => {
-    const [food, setFood] = useState('');
+  const [foods, setFoods]: any = useState([]);
 
-    // const nextFoodFilter = nextFood.filter((food, idx) => Math.floor(Math.random() * nextFood.length + 1));
+  const foodRandom = nextFood[Math.floor(Math.random() * nextFood.length + 1)];
+  const checkFoodRandom = foods[Math.floor(Math.random() * foods.length + 1)];
+  console.log(foods);
+  console.log(checkFoodRandom);
 
-    // const returnName = nextFood.map((food, idx) => {
-    //     if (idx === Math.floor(Math.random() * nextFood.length + 1)) {
-    //         return (
-    //             <div key={idx}>
-    //                 {idx + 1}.{food.name} - {food.kind}
-    //             </div>
-    //         );
-    //     }
-    //     return;
-    // });
-    const foodRandom = nextFood[Math.floor(Math.random() * nextFood.length + 1)];
+  const removeFood = (e: any) => {
+    console.log(e.target.getAttribute("name"));
 
-    return (
-        <Rayout>
-            <Header />
-            <Main>
-                <div>
-                    {nextFood.map((food, idx): any => {
-                        return (
-                            <FoodInfo key={idx}>
-                                <div>{idx + 1} .</div>
-                                <div>{food.name}</div>
-                                <div>{food.kind}</div>
-                            </FoodInfo>
-                        );
-                    })}
-                </div>
-                <>
-                    <ResultFoodBox>
-                        <p>점심 랜덤으로 고르기</p>
-                        <Button variant="outlined" color="secondary" style={{ height: '50px', width: '200px' }}>
-                            누르기
-                        </Button>
-                        <h2>
-                            {foodRandom.id}. {foodRandom.name} -{foodRandom.kind}
-                        </h2>
-                    </ResultFoodBox>
-                </>
-            </Main>
-        </Rayout>
-    );
+    const name = e.target.getAttribute("name");
+    setFoods(foods.filter((item: { name: any }) => item.name !== name));
+  };
+  return (
+    <Rayout>
+      <Main>
+        <div>
+          <h2>전체 메뉴</h2>
+          {nextFood.map((food, idx): any => {
+            return (
+              <FoodInfo key={idx}>
+                <input
+                  type="checkbox"
+                  value={food.name}
+                  onChange={(e) => {
+                    setFoods([...foods, e.target.value]);
+                  }}
+                ></input>
+                <div>{idx + 1} .</div>
+                <div>{food.name}</div>
+                <div>{food.kind}</div>
+              </FoodInfo>
+            );
+          })}
+        </div>
+      </Main>
+      <div>
+        <Header />
+        <ResultFoodBox>
+          <p>점심 랜덤으로 고르기</p>
+          <Button
+            variant="outlined"
+            color="secondary"
+            style={{ height: "50px", width: "200px" }}
+            onClick={checkFoodRandom}
+          >
+            누르기
+          </Button>
+          {/* <h2>
+            {foodRandom.id}. {foodRandom.name} -{foodRandom.kind}
+          </h2> */}
+          {checkFoodRandom}
+        </ResultFoodBox>
+      </div>
+      <div>
+        {foods.map((food: any, idx: number) => {
+          return (
+            <div id={food.id} key={idx} style={{ display: "flex" }}>
+              <div>{food}</div>
+              <Button onClick={removeFood}>X</Button>
+            </div>
+          );
+        })}
+      </div>
+    </Rayout>
+  );
 };
 const ResultFoodBox = styled.div`
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 `;
 const Main = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    width: auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  width: auto;
 `;
 const FoodInfo = styled.div`
-    display: flex;
-    gap: 15px;
+  display: flex;
+  gap: 15px;
 `;
 
 const Rayout = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  display: flex;
+  align-items: center;
+  width: 100%;
 `;
 
 export default App;
