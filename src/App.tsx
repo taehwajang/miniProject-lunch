bc51dd2f93812a0fff297472bc45f63c3c1c1ab8
taehwajang <61370482+taehwajang@users.noreply.github.com>
2022-04-16 15:50:54 +0900

    add : 체크메뉴 선택

--- src/App.tsx
+++ src/App.tsx
@@ -1,97 +1,107 @@
-import { Button } from "@mui/material";
+import { Button } from '@mui/material';
 
-import React, { useState } from "react";
-import styled from "styled-components";
-import Header from "./component/header";
-import { nextFood } from "./data/data";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Header from './component/header';
+import { nextFood } from './data/data';
 
 const App = () => {
-  const [foods, setFoods]: any = useState([]);
+    const [count, setCount] = useState(1);
+    const [foods, setFoods]: any = useState([]);
+    const [foodRandomIsOpen, setFoodRandomIsOpen] = useState(false);
+    const foodRandom = nextFood[Math.floor(Math.random() * nextFood.length + count)];
+    const checkFoodRandom = foods[Math.floor(Math.random() * nextFood.length + 1)];
+    console.log(checkFoodRandom);
 
-  const foodRandom = nextFood[Math.floor(Math.random() * nextFood.length + 1)];
-  const checkFoodRandom = foods[Math.floor(Math.random() * foods.length + 1)];
-  console.log(foods);
-  console.log(checkFoodRandom);
+    const [checkedFoods, setCheckedFoods]: any = useState([]);
 
-  const removeFood = (e: any) => {
-    console.log(e.target.getAttribute("name"));
-
-    const name = e.target.getAttribute("name");
-    setFoods(foods.filter((item: { name: any }) => item.name !== name));
-  };
-  return (
-    <Rayout>
-      <Main>
-        <div>
-          <h2>전체 메뉴</h2>
-          {nextFood.map((food, idx): any => {
-            return (
-              <FoodInfo key={idx}>
-                <input
-                  type="checkbox"
-                  value={food.name}
-                  onChange={(e) => {
-                    setFoods([...foods, e.target.value]);
-                  }}
-                ></input>
-                <div>{idx + 1} .</div>
-                <div>{food.name}</div>
-                <div>{food.kind}</div>
-              </FoodInfo>
-            );
-          })}
-        </div>
-      </Main>
-      <div>
-        <Header />
-        <ResultFoodBox>
-          <p>점심 랜덤으로 고르기</p>
-          <Button
-            variant="outlined"
-            color="secondary"
-            style={{ height: "50px", width: "200px" }}
-            onClick={checkFoodRandom}
-          >
-            누르기
-          </Button>
-          {/* <h2>
-            {foodRandom.id}. {foodRandom.name} -{foodRandom.kind}
-          </h2> */}
-          {checkFoodRandom}
-        </ResultFoodBox>
-      </div>
-      <div>
-        {foods.map((food: any, idx: number) => {
-          return (
-            <div id={food.id} key={idx} style={{ display: "flex" }}>
-              <div>{food}</div>
-              <Button onClick={removeFood}>X</Button>
-            </div>
-          );
-        })}
-      </div>
-    </Rayout>
-  );
+    const removeContent = (e: any) => {
+        const name = e.target.id;
+        setCheckedFoods(checkedFoods.filter((e: any) => e !== name));
+    };
+    return (
+        <>
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', fontSize: '40px', color: 'purple' }}>오늘 뭐 먹을래?</div>
+            <Rayout>
+                <Main>
+                    <div style={{ border: '1px solid red' }}>
+                        <div style={{ fontSize: '30px' }}>전체 메뉴</div>
+                        {nextFood.map((food, idx): any => {
+                            return (
+                                <FoodInfo key={idx}>
+                                    <input
+                                        type="checkbox"
+                                        value={food.name}
+                                        onChange={(e) => {
+                                            e.target.checked ? setCheckedFoods([...checkedFoods, e.target.value]) : setCheckedFoods(checkedFoods.filter((item: any) => item !== e.target.value));
+                                            // setFoods([...foods, e.target.value]);
+                                        }}
+                                    ></input>
+                                    <div>{food.name}</div>
+                                    <div>{food.kind}</div>
+                                </FoodInfo>
+                            );
+                        })}
+                    </div>
+                </Main>
+                <div>
+                    <ResultFoodBox>
+                        <p>점심 랜덤으로 고르기</p>
+                        <Button
+                            variant="outlined"
+                            color="secondary"
+                            style={{ height: '50px', width: '200px' }}
+                            onClick={() => {
+                                setFoodRandomIsOpen(!foodRandomIsOpen);
+                                setCount(count);
+                            }}
+                        >
+                            돌리기
+                        </Button>
+                        {foodRandomIsOpen === true ? foodRandom.name : null}
+                    </ResultFoodBox>
+                </div>
+                <div>
+                    <div>선택한 메뉴만 고르기</div>
+                    <Button variant="outlined" style={{ width: '150px', height: '50px' }}>
+                        돌리기
+                    </Button>
+                    <div>
+                        {checkedFoods.map((food: any, idx: number) => {
+                            return (
+                                <div id={food.id} key={idx} style={{ display: 'flex' }}>
+                                    <div>{food}</div>
+                                    <Button id={food} onClick={removeContent}>
+                                        X
+                                    </Button>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+            </Rayout>
+        </>
+    );
 };
 const ResultFoodBox = styled.div`
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
 `;
-const Main = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  width: auto;
+const Main = styled.label`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    width: auto;
 `;
-const FoodInfo = styled.div`
-  display: flex;
-  gap: 15px;
+const FoodInfo = styled.label`
+    display: flex;
+    gap: 15px;
 `;
 
 const Rayout = styled.div`
-  display: flex;
-  align-items: center;
-  width: 100%;
+    display: flex;
+    width: 100%;
+    gap: 30px;
 `;
 
 export default App;
--- src/component/menu.tsx
+++ src/component/menu.tsx
@@ -1,23 +1,18 @@
-import { Input } from '@mui/material'
-import React from 'react'
+import { Input } from '@mui/material';
+import React from 'react';
 
+const Menu = ({ data }: any) => {
+    return (
+        <div>
+            {data.map((food: { name: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; number: string }) => {
+                return (
+                    <>
+                        <div>{food?.number}</div>
+                    </>
+                );
+            })}
+        </div>
+    );
+};
 
-const Menu = ({data}:any) => {
-  return (
-    <div>
-      
-     
-        {
-        data.map((food: { name: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined ,number:string}) =>{
-            return <>
-<div>{food?.number}</div>
-            </>
-
-        })
-        
-        }
-      </div>
-  )
-}
-
-export default Menu
+export default Menu;
